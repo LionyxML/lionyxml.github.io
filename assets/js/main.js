2b909cab2fe9876eb9490fa4e207a2b7930128db
LionyxML <rahul.juliato@gmail.com>
2021-04-12 15:47:20 -0300

    Better menu

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-
-// ANIMAÇÕES PELO PLUGIN gsap
-// gsap.from('.seta', { opacity: 0, duration: 1, delay: 2.5, y: 10, scale: 2})
-// gsap.from('.nav-item',  { opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2})
-// gsap.from('.home-titulo',  { opacity: 0, duration: 1, delay: 1.8, y: 30})
-// gsap.from('.home-descricao',  { opacity: 0, duration: 1, delay: 1.8, y: 30})
-// gsap.from('.home-botao',  { opacity: 0, duration: 1, delay: 1.8, y: 30})
-// gsap.from('.home-img',  { opacity: 0, duration: 1, delay: 1.3, y: 30})

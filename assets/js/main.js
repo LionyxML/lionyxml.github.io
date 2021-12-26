3319da7df626ade846f7f9e39cbea51bb5bdc95b
LionyxML <rahul.juliato@gmail.com>
2021-12-25 22:40:25 -0300

    Added R-QR

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,71 @@
-var modalApi = document.getElementsByClassName('modal-api')[0];
-var btnApi = document.getElementById('btn-api');
-var btnApiClose = document.getElementById('fechar-api');
+var modalApi = document.getElementsByClassName("modal-api")[0];
+var btnApi = document.getElementById("btn-api");
+var btnApiClose = document.getElementById("fechar-api");
 
-btnApi.addEventListener('click', function () {
-  modalApi.classList.toggle('modal-mostrar');
+btnApi.addEventListener("click", function () {
+  modalApi.classList.toggle("modal-mostrar");
 });
 
-btnApiClose.addEventListener('click', function () {
-  modalApi.classList.remove('modal-mostrar');
+btnApiClose.addEventListener("click", function () {
+  modalApi.classList.remove("modal-mostrar");
 });
 
-var modalLan = document.getElementsByClassName('modal-land')[0];
-var btnLan = document.getElementById('btn-land');
-var btnLanClose = document.getElementById('fechar-land');
+var modalLan = document.getElementsByClassName("modal-land")[0];
+var btnLan = document.getElementById("btn-land");
+var btnLanClose = document.getElementById("fechar-land");
 
-btnLan.addEventListener('click', function () {
-  modalLan.classList.toggle('modal-mostrar');
+btnLan.addEventListener("click", function () {
+  modalLan.classList.toggle("modal-mostrar");
 });
 
-btnLanClose.addEventListener('click', function () {
-  modalLan.classList.remove('modal-mostrar');
+btnLanClose.addEventListener("click", function () {
+  modalLan.classList.remove("modal-mostrar");
 });
 
-var modalMic = document.getElementsByClassName('modal-micro')[0];
-var btnMic = document.getElementById('btn-micro');
-var btnMicClose = document.getElementById('fechar-micro');
+var modalQR = document.getElementsByClassName("modal-qr")[0];
+var btnQR = document.getElementById("btn-r-qr");
+var btnQRClose = document.getElementById("fechar-qr");
 
-btnMic.addEventListener('click', function () {
-  modalMic.classList.toggle('modal-mostrar');
+btnQR.addEventListener("click", function () {
+  modalQR.classList.toggle("modal-mostrar");
 });
 
-btnMicClose.addEventListener('click', function () {
-  modalMic.classList.remove('modal-mostrar');
+btnQRClose.addEventListener("click", function () {
+  modalQR.classList.remove("modal-mostrar");
 });
 
-var modalDash = document.getElementsByClassName('modal-dash')[0];
-var btnDash = document.getElementById('btn-dash');
-var btnDashClose = document.getElementById('fechar-dash');
+var modalMic = document.getElementsByClassName("modal-micro")[0];
+var btnMic = document.getElementById("btn-micro");
+var btnMicClose = document.getElementById("fechar-micro");
 
-btnDash.addEventListener('click', function () {
-  modalDash.classList.toggle('modal-mostrar');
+btnMic.addEventListener("click", function () {
+  modalMic.classList.toggle("modal-mostrar");
 });
 
-btnDashClose.addEventListener('click', function () {
-  modalDash.classList.remove('modal-mostrar');
+btnMicClose.addEventListener("click", function () {
+  modalMic.classList.remove("modal-mostrar");
 });
 
-var modalPort = document.getElementsByClassName('modal-landprof')[0];
-var btnPort = document.getElementById('btn-landprof');
-var btnPortClose = document.getElementById('fechar-landprof');
+var modalDash = document.getElementsByClassName("modal-dash")[0];
+var btnDash = document.getElementById("btn-dash");
+var btnDashClose = document.getElementById("fechar-dash");
 
-btnPort.addEventListener('click', function () {
-  modalPort.classList.toggle('modal-mostrar');
+btnDash.addEventListener("click", function () {
+  modalDash.classList.toggle("modal-mostrar");
 });
 
-btnPortClose.addEventListener('click', function () {
-  modalPort.classList.remove('modal-mostrar');
+btnDashClose.addEventListener("click", function () {
+  modalDash.classList.remove("modal-mostrar");
+});
+
+var modalPort = document.getElementsByClassName("modal-landprof")[0];
+var btnPort = document.getElementById("btn-landprof");
+var btnPortClose = document.getElementById("fechar-landprof");
+
+btnPort.addEventListener("click", function () {
+  modalPort.classList.toggle("modal-mostrar");
+});
+
+btnPortClose.addEventListener("click", function () {
+  modalPort.classList.remove("modal-mostrar");
 });

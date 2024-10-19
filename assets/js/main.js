297b99ae2be3b08291e75aa65505278fdaca8bd8
Rahul Martim Juliato <rahul.juliato@gmail.com>
2024-10-18 22:06:46 -0300

    refactor: new content in english

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,95 @@
 var modalApi = document.getElementsByClassName("modal-api")[0];
 var btnApi = document.getElementById("btn-api");
-var btnApiClose = document.getElementById("fechar-api");
+var btnApiClose = document.getElementById("close-api");
 
 btnApi.addEventListener("click", function () {
-  modalApi.classList.toggle("modal-mostrar");
+  modalApi.classList.toggle("modal-show");
 });
 
 btnApiClose.addEventListener("click", function () {
-  modalApi.classList.remove("modal-mostrar");
+  modalApi.classList.remove("modal-show");
 });
 
 var modalLan = document.getElementsByClassName("modal-land")[0];
 var btnLan = document.getElementById("btn-land");
-var btnLanClose = document.getElementById("fechar-land");
+var btnLanClose = document.getElementById("close-land");
 
 btnLan.addEventListener("click", function () {
-  modalLan.classList.toggle("modal-mostrar");
+  modalLan.classList.toggle("modal-show");
 });
 
 btnLanClose.addEventListener("click", function () {
-  modalLan.classList.remove("modal-mostrar");
+  modalLan.classList.remove("modal-show");
 });
 
 var modalQR = document.getElementsByClassName("modal-qr")[0];
 var btnQR = document.getElementById("btn-r-qr");
-var btnQRClose = document.getElementById("fechar-qr");
+var btnQRClose = document.getElementById("close-qr");
 
 btnQR.addEventListener("click", function () {
-  modalQR.classList.toggle("modal-mostrar");
+  modalQR.classList.toggle("modal-show");
 });
 
 btnQRClose.addEventListener("click", function () {
-  modalQR.classList.remove("modal-mostrar");
+  modalQR.classList.remove("modal-show");
 });
 
 var modalMic = document.getElementsByClassName("modal-micro")[0];
 var btnMic = document.getElementById("btn-micro");
-var btnMicClose = document.getElementById("fechar-micro");
+var btnMicClose = document.getElementById("close-micro");
 
 btnMic.addEventListener("click", function () {
-  modalMic.classList.toggle("modal-mostrar");
+  modalMic.classList.toggle("modal-show");
 });
 
 btnMicClose.addEventListener("click", function () {
-  modalMic.classList.remove("modal-mostrar");
+  modalMic.classList.remove("modal-show");
 });
 
 var modalDash = document.getElementsByClassName("modal-dash")[0];
 var btnDash = document.getElementById("btn-dash");
-var btnDashClose = document.getElementById("fechar-dash");
+var btnDashClose = document.getElementById("close-dash");
 
 btnDash.addEventListener("click", function () {
-  modalDash.classList.toggle("modal-mostrar");
+  modalDash.classList.toggle("modal-show");
 });
 
 btnDashClose.addEventListener("click", function () {
-  modalDash.classList.remove("modal-mostrar");
+  modalDash.classList.remove("modal-show");
 });
 
 var modalPort = document.getElementsByClassName("modal-landprof")[0];
 var btnPort = document.getElementById("btn-landprof");
-var btnPortClose = document.getElementById("fechar-landprof");
+var btnPortClose = document.getElementById("close-landprof");
 
 btnPort.addEventListener("click", function () {
-  modalPort.classList.toggle("modal-mostrar");
+  modalPort.classList.toggle("modal-show");
 });
 
 btnPortClose.addEventListener("click", function () {
-  modalPort.classList.remove("modal-mostrar");
+  modalPort.classList.remove("modal-show");
+});
+
+var modalGame = document.getElementsByClassName("modal-game")[0];
+var btnPort = document.getElementById("btn-game");
+var btnPortClose = document.getElementById("close-game");
+
+btnPort.addEventListener("click", function () {
+  modalGame.classList.toggle("modal-show");
+});
+
+btnPortClose.addEventListener("click", function () {
+  modalGame.classList.remove("modal-show");
+});
+
+var modalBlog = document.getElementsByClassName("modal-blog")[0];
+var btnPort = document.getElementById("btn-blog");
+var btnPortClose = document.getElementById("close-blog");
+
+btnPort.addEventListener("click", function () {
+  modalBlog.classList.toggle("modal-show");
+});
+
+btnPortClose.addEventListener("click", function () {
+  modalBlog.classList.remove("modal-show");
 });
